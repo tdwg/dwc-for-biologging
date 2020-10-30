dcdfb2443e5291423d40f3a14d2a8b5ed61dc733
peterdesmet <peter.desmet@inbo.be>
2020-10-30 15:53:23 +0100

    Express camtrap-dp as occ + multimedia, rather than event + occ
    - Drop dwc_event.sql
    - Make dwc_occurrence.sql a core
    - Add dwc_multimedia.sql as extension

---
+++ use-cases/camtrap-dp/sql/dwc_multimedia.sql
@@ -0,0 +1,53 @@
+/*
+Created by Peter Desmet (INBO)
+
+This query transforms camtrap-dp image data to a Darwin Core Multimedia extension. Sets of images 
+will appear multiple times if these are used for multiple occurrences.
+
+Static Darwin Core values are marked with FIXED VALUE.
+
+MEDIA
+
+media_id                                TODO: add as link to observation for non-sequenced based
+deployment_id                           N: included at observation level
+sequence_id                             Y: as link to observation
+timestamp                               Y
+file_path                               Y
+file_mediatype                          Y
+
+*/
+
+SELECT
+-- occurrenceID
+  obs."observation_id" AS "occurrenceID",
+-- type
+  CASE
+    WHEN media."file_mediatype" LIKE '%video%' THEN 'MovingImage'
+    ELSE 'StillImage'
+  END AS "type",
+-- format
+  media."file_mediatype" AS "format",
+-- identifier
+  media."file_path" AS "identifier",
+-- references
+-- title
+-- description
+-- created
+  STRFTIME('%Y-%m-%dT%H:%M:%SZ', media."timestamp") AS "created"
+-- creator
+-- contributor
+-- publisher
+-- audience
+-- source
+-- license                              Only available in dataset metadata
+-- rightsHolder                         Only available in dataset metadata
+-- datasetID                            Only available in dataset metadata
+
+FROM
+  observations AS obs
+  LEFT JOIN media as media
+  ON
+    obs."sequence_id" = media."sequence_id"
+
+WHERE
+  obs."observation_type" = 'animal'
