b010819e9abfe995b77a63686bfd4a5d169d71b8
peterdesmet <peter.desmet@inbo.be>
2020-12-17 15:20:11 +0100

    Extend multimedia SQL to full camtrap-dp: make it work for sequence and image based + sort

--- use-cases/camtrap-dp/sql/dwc_multimedia.sql
+++ use-cases/camtrap-dp/sql/dwc_multimedia.sql
@@ -6,32 +6,66 @@ Static Darwin Core values are marked with FIXED VALUE.
 
 MEDIA
 
-media_id                                TODO: add as link to observation for non-sequenced based
+multimedia_id                           Y: as link to observation
 deployment_id                           N: included at observation level
 sequence_id                             Y: as link to observation
 timestamp                               Y
 file_path                               Y
+file_name                               N
 file_mediatype                          Y
+exif_data                               N
+comments                                N
+_id                                     N
 
 */
 
+-- Observations can be based on sequences (sequence_id) or individual files (multimedia_id)
+-- Make two joins and union to capture both cases
+WITH obs_multimedia AS (
+-- Sequence based observations
+  SELECT
+    obs."observation_id",
+    mm.*
+  FROM
+    observations AS obs
+    LEFT JOIN
+      multimedia AS mm
+      ON obs."sequence_id" = mm."sequence_id"
+  WHERE
+    obs."multimedia_id" IS NULL
+    AND obs."observation_type" = 'animal'
+  UNION
+-- Multimedia based observations
+  SELECT
+    obs."observation_id",
+    mm.*
+  FROM
+    observations AS obs
+    LEFT JOIN
+      multimedia AS mm
+      ON obs."multimedia_id" = mm."multimedia_id"
+  WHERE
+    obs."multimedia_id" IS NOT NULL
+    AND obs."observation_type" = 'animal'
+)
+
 SELECT
 -- occurrenceID
-  obs."observation_id" AS "occurrenceID",
+  obs_mm."observation_id" AS "occurrenceID",
 -- type
   CASE
-    WHEN media."file_mediatype" LIKE '%video%' THEN 'MovingImage'
+    WHEN obs_mm."file_mediatype" LIKE '%video%' THEN 'MovingImage'
     ELSE 'StillImage'
   END AS "type",
 -- format
-  media."file_mediatype" AS "format",
+  obs_mm."file_mediatype" AS "format",
 -- identifier
-  media."file_path" AS "identifier",
+  obs_mm."file_path" AS "identifier",
 -- references
 -- title
 -- description
 -- created
-  STRFTIME('%Y-%m-%dT%H:%M:%SZ', media."timestamp") AS "created"
+  STRFTIME('%Y-%m-%dT%H:%M:%SZ', obs_mm."timestamp") AS "created"
 -- creator
 -- contributor
 -- publisher
@@ -42,10 +76,10 @@ SELECT
 -- datasetID                            Only available in dataset metadata
 
 FROM
-  observations AS obs
-  LEFT JOIN media as media
-  ON
-    obs."sequence_id" = media."sequence_id"
+  obs_multimedia AS obs_mm
 
-WHERE
-  obs."observation_type" = 'animal'
+ORDER BY
+-- Order is not retained in obs_multimedia, so important to sort
+  obs_mm."observation_id",
+  obs_mm."timestamp",
+  obs_mm."file_name"
